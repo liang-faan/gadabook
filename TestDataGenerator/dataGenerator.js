7523550f1ea85aba1e80d10180c86876acad1a3c
stonefruit <jeromeng888@gmail.com>
2019-09-14 14:45:17 +0800

    update partition and sort keys for mock data

--- TestDataGenerator/dataGenerator.js
+++ TestDataGenerator/dataGenerator.js
@@ -1,274 +1,317 @@
-var faker = require('faker');
-var helpers = require('./helpers');
+var faker = require("faker")
+var helpers = require("./helpers")
 
-var currentEpochTime = Math.floor(new Date() / 1000);
-var secondsInADay = 86400;
+var currentEpochTime = Math.floor(new Date() / 1000)
+var secondsInADay = 86400
 
-var users = {};
-var tags = {};
-var availabilities = {};
-var catalogues = {};
-var bookings = {};
-var enrollments = {};
+var users = {}
+var tags = {}
+var availabilities = {}
+var catalogues = {}
+var bookings = {}
+var enrollments = {}
 
 function generateUniqueUser() {
   var newUser = {
-    "UserID": {
-      S: faker.random.uuid()
+    partitionKey: {
+      S: `User_${faker.random.uuid()}`
     },
-    "Role": {
-      S: 'normal'
+    sortKey: {
+      S: `User_${faker.random.uuid()}`
     },
-    "Name": {
+    Role: {
+      S: "normal"
+    },
+    Name: {
       S: faker.internet.userName()
     },
-	"FirstName": {
+    FirstName: {
       S: faker.name.firstName()
     },
-    "LastName": {
+    LastName: {
       S: faker.name.lastName()
     },
-    "Email": {
+    Email: {
       S: faker.internet.email()
     },
-	"Phone": {
+    Phone: {
       S: faker.phone.phoneNumber()
     },
-	"DOB": {
-      N: faker.random.number({
-        'min': currentEpochTime - secondsInADay * 365 * 40,
-        'max': currentEpochTime - secondsInADay * 365 * 18
-      }).toString()
+    DOB: {
+      N: faker.random
+        .number({
+          min: currentEpochTime - secondsInADay * 365 * 40,
+          max: currentEpochTime - secondsInADay * 365 * 18
+        })
+        .toString()
     },
-	"Gender": {
+    Gender: {
       S: "Male"
     },
-	"Address": {
+    Address: {
       S: faker.address.streetAddress()
     },
-	"UserStatus": {
+    UserStatus: {
       S: "ok"
     },
-	"Active": {
+    Active: {
       S: "ok"
     }
-  };
+  }
 
-  users[newUser.UserID.S] = newUser;
-  helpers.printPretty(newUser);
-  return newUser;
+  users[newUser.UserID.S] = newUser
+  helpers.printPretty(newUser)
+  return newUser
 }
 
 function generateUniqueTag() {
   var newTag = {
-    "TagID": {
-      S: faker.random.uuid()
+    partitionKey: {
+      S: `Tag_${faker.random.uuid()}`
     },
-    "Description": {
+    sortKey: {
+      S: `Tag_${faker.random.uuid()}`
+    },
+    Description: {
       S: faker.lorem.paragraphs(1)
     },
-	"Active": {
+    Active: {
       S: "ok"
     }
-  };
+  }
 
-  tags[newTag.TagID.S] = newTag;
-  helpers.printPretty(newTag);
-  return newTag;
+  tags[newTag.TagID.S] = newTag
+  helpers.printPretty(newTag)
+  return newTag
 }
 
 function generateUniqueAvailability() {
   var newAvailability = {
-    "AvailabilityID": {
-      S: faker.random.uuid()
+    partitionKey: {
+      S: `Availability_${faker.random.uuid()}`
     },
-    "Date": {
-      N: faker.random.number({
-        'min': currentEpochTime,
-        'max': currentEpochTime + secondsInADay * 18
-      }).toString()
+    sortKey: {
+      S: `Availability_${faker.random.uuid()}`
     },
-	"Slot": {
-      N: faker.random.number({
-        'min': 1,
-        'max': 5
-      }).toString()
+    Date: {
+      N: faker.random
+        .number({
+          min: currentEpochTime,
+          max: currentEpochTime + secondsInADay * 18
+        })
+        .toString()
+    },
+    Slot: {
+      N: faker.random
+        .number({
+          min: 1,
+          max: 5
+        })
+        .toString()
     }
-  };
+  }
 
-  availabilities[newAvailability.AvailabilityID.S] = newAvailability;
-  helpers.printPretty(newAvailability);
-  return newAvailability;
+  availabilities[newAvailability.AvailabilityID.S] = newAvailability
+  helpers.printPretty(newAvailability)
+  return newAvailability
 }
 
 function generateUniqueCatalogue(tag, availability) {
-	var variables = {
-		Venue: 'Room 2',
-		Type: 'Facility',
-		City: faker.address.city,
-		Address: faker.address.streetAddress
-	};
+  var variables = {
+    Venue: "Room 2",
+    Type: "Facility",
+    City: faker.address.city,
+    Address: faker.address.streetAddress
+  }
   var newCatalogue = {
-    "CatalogueID": {
-      S: tag.TagID.S
+    partitionKey: {
+      S: `Catalogue_${tag.TagID.S}`
     },
-	"TagID": {
+    sortKey: {
+      S: `Catalogue_${tag.TagID.S}`
+    },
+    TagID: {
       S: availability.AvailabilityID.S
     },
-	"AvailabilityID": {
+    AvailabilityID: {
       S: faker.random.uuid()
     },
-    "Name": {
+    Name: {
       S: faker.lorem.word(5)
     },
-    "Currency": {
+    Currency: {
       S: "SGD"
     },
-    "TermsCondition": {
+    TermsCondition: {
       S: faker.lorem.paragraphs(1)
     },
-    "Rate": {  
-      S: faker.random.number({
-        'min': 10,
-        'max': 100
-      }).toString()
+    Rate: {
+      S: faker.random
+        .number({
+          min: 10,
+          max: 100
+        })
+        .toString()
     },
-    "Variables": {  
+    Variables: {
       S: JSON.stringify(variables)
     }
-  };
+  }
 
-  catalogues[newCatalogue.CatalogueID.S] = newCatalogue;
-  helpers.printPretty(newCatalogue);
-  return newCatalogue;
+  catalogues[newCatalogue.CatalogueID.S] = newCatalogue
+  helpers.printPretty(newCatalogue)
+  return newCatalogue
 }
 
 function generateUniqueBooking(user, catalogue) {
   var newBooking = {
-    "BookingID": {
-      S: faker.random.uuid()
+    partitionKey: {
+      S: `Booking_${faker.random.uuid()}`
     },
-	"CatalogueID": {
+    sortKey: {
+      S: `Booking_${faker.random.uuid()}`
+    },
+    CatalogueID: {
       S: catalogue.CatalogueID.S
     },
-	"UserID": {
+    UserID: {
       S: user.UserID.S
     },
-	"TransactionID": {
+    TransactionID: {
       S: faker.random.uuid()
     },
-    "Status": {
+    Status: {
       S: "ok"
     },
-    "StartTime": {  
-      N: faker.random.number({
-        'min': currentEpochTime - (secondsInADay * 10),
-        'max': currentEpochTime - secondsInADay
-      }).toString()
+    StartTime: {
+      N: faker.random
+        .number({
+          min: currentEpochTime - secondsInADay * 10,
+          max: currentEpochTime - secondsInADay
+        })
+        .toString()
     },
-	"EndTime": {  
-      N: faker.random.number({
-        'min': currentEpochTime + secondsInADay,
-        'max': currentEpochTime + (secondsInADay * 10)
-      }).toString()
+    EndTime: {
+      N: faker.random
+        .number({
+          min: currentEpochTime + secondsInADay,
+          max: currentEpochTime + secondsInADay * 10
+        })
+        .toString()
     },
-    "Active": {
+    Active: {
       S: "ok"
     },
-    "Amount": {  
-      S: faker.random.number({
-        'min': 10,
-        'max': 100
-      }).toString()
+    Amount: {
+      S: faker.random
+        .number({
+          min: 10,
+          max: 100
+        })
+        .toString()
     }
-  };
+  }
 
-  bookings[newBooking.BookingID.S] = newBooking;
-  helpers.printPretty(newBooking);
-  return newBooking;
+  bookings[newBooking.BookingID.S] = newBooking
+  helpers.printPretty(newBooking)
+  return newBooking
 }
 
 function generateUniqueEnrollment(user, catalogue) {
   var newEnrollment = {
-    "EnrollmentID": {
-      S: faker.random.uuid()
+    partitionKey: {
+      S: `Enrollment_${faker.random.uuid()}`
     },
-	"CatalogueID": {
+    sortKey: {
+      S: `Enrollment_${faker.random.uuid()}`
+    },
+    CatalogueID: {
       S: catalogue.CatalogueID.S
     },
-	"UserID": {
+    UserID: {
       S: user.UserID.S
     },
-	"TransactionID": {
+    TransactionID: {
       S: faker.random.uuid()
     },
-	"ExpiryDate": {  
-      N: faker.random.number({
-        'min': currentEpochTime + secondsInADay,
-        'max': currentEpochTime + (secondsInADay * 10)
-      }).toString()
+    ExpiryDate: {
+      N: faker.random
+        .number({
+          min: currentEpochTime + secondsInADay,
+          max: currentEpochTime + secondsInADay * 10
+        })
+        .toString()
     },
-    "Active": {
+    Active: {
       S: "ok"
     },
-    "Fee": {  
-      S: faker.random.number({
-        'min': 10,
-        'max': 100
-      }).toString()
+    Fee: {
+      S: faker.random
+        .number({
+          min: 10,
+          max: 100
+        })
+        .toString()
     }
-  };
+  }
 
-  enrollments[newEnrollment.EnrollmentID.S] = newEnrollment;
-  helpers.printPretty(newEnrollment);
-  return newEnrollment;
+  enrollments[newEnrollment.EnrollmentID.S] = newEnrollment
+  helpers.printPretty(newEnrollment)
+  return newEnrollment
 }
 
-function generateAllData(numberOfCatalogues, numberOfUsers, numberOfTags, numberOfAvailabilties, numberOfBookings, numberOfEnrollments) {
-  for (var i=0; i<numberOfUsers; i++) {
-    generateUniqueUser();
-	console.log('Generated', 'User');
+function generateAllData(
+  numberOfCatalogues,
+  numberOfUsers,
+  numberOfTags,
+  numberOfAvailabilties,
+  numberOfBookings,
+  numberOfEnrollments
+) {
+  for (var i = 0; i < numberOfUsers; i++) {
+    generateUniqueUser()
+    console.log("Generated", "User")
   }
-  
-  for (var i=0; i<numberOfTags; i++) {
-    generateUniqueTag();
-	console.log('Generated', 'Tag');
+
+  for (var i = 0; i < numberOfTags; i++) {
+    generateUniqueTag()
+    console.log("Generated", "Tag")
   }
-  
-  for (var i=0; i<numberOfAvailabilties; i++) {
-    generateUniqueAvailability();
-	console.log('Generated', 'Availability');
+
+  for (var i = 0; i < numberOfAvailabilties; i++) {
+    generateUniqueAvailability()
+    console.log("Generated", "Availability")
   }
-  
-  for (var i=0; i<numberOfCatalogues; i++) {
-	var tag = faker.random.objectElement(tags);
-	var availability = faker.random.objectElement(availabilities);
-    generateUniqueCatalogue(tag, availability);
-	console.log('Generated', 'Catalogue');
+
+  for (var i = 0; i < numberOfCatalogues; i++) {
+    var tag = faker.random.objectElement(tags)
+    var availability = faker.random.objectElement(availabilities)
+    generateUniqueCatalogue(tag, availability)
+    console.log("Generated", "Catalogue")
   }
-  
-  for (var i=0; i<numberOfBookings; i++) {
-	var user = faker.random.objectElement(users);
-	var catalogue = faker.random.objectElement(catalogues);
-    generateUniqueBooking(user, catalogue);
-	console.log('Generated', 'Bookings');
+
+  for (var i = 0; i < numberOfBookings; i++) {
+    var user = faker.random.objectElement(users)
+    var catalogue = faker.random.objectElement(catalogues)
+    generateUniqueBooking(user, catalogue)
+    console.log("Generated", "Bookings")
   }
-  
-  for (var i=0; i<numberOfEnrollments; i++) {
-	var user = faker.random.objectElement(users);
-	var catalogue = faker.random.objectElement(catalogues);
-    generateUniqueEnrollment(user, catalogue);
-	console.log('Generated', 'Enrollment');
+
+  for (var i = 0; i < numberOfEnrollments; i++) {
+    var user = faker.random.objectElement(users)
+    var catalogue = faker.random.objectElement(catalogues)
+    generateUniqueEnrollment(user, catalogue)
+    console.log("Generated", "Enrollment")
   }
 
   return {
-    'Gardabook.User': users,
-    'Gardabook.Tag': tags,
-	'Gardabook.Availability': availabilities,
-	'Gardabook.Catalogue': catalogues,
-	'Gardabook.Bookings': bookings,
-	'Gardabook.Enrollment': enrollments
-  };
+    "Gardabook.User": users,
+    "Gardabook.Tag": tags,
+    "Gardabook.Availability": availabilities,
+    "Gardabook.Catalogue": catalogues,
+    "Gardabook.Bookings": bookings,
+    "Gardabook.Enrollment": enrollments
+  }
 }
 
-module.exports.generateAllData = generateAllData;
+module.exports.generateAllData = generateAllData
--- TestDataGenerator/generateData.js
+++ TestDataGenerator/generateData.js
@@ -1,142 +1,146 @@
-var AWS = require('aws-sdk');
-var gen = require('./dataGenerator');
-var async = require('async');
+// Libraries
+var AWS = require("aws-sdk")
+var async = require("async")
 
-/*
-content of config.json
-{ 
-    "accessKeyId": "xxx", 
-    "secretAccessKey": "xxxxx", 
-    "region": "xxx" 
-}
-*/
-AWS.config.loadFromPath('./../../config.json');
+// Code
+var gen = require("./dataGenerator")
 
-var dynamodb = new AWS.DynamoDB();
+// AWS.config not required if 'aws configure' has been done in the cli as the sdk will automatically use it.
+
+var dynamodb = new AWS.DynamoDB()
+var tableName = "gardabook-sample"
 
 //numberOfCatalogues, numberOfUsers, numberOfTags, numberOfAvailabilties, numberOfBookings, numberOfEnrollments
-var allData = gen.generateAllData(5, 10, 2, 3, 5, 3);
+var allData = gen.generateAllData(5, 10, 2, 3, 5, 3)
 
 // SETUP WORKLOADS
 var work = [].concat(
-	function(done) { 
-        var tableName = 'Gardabook.User';
-        console.log('Generated',Object.keys(allData[tableName]).length, 'User');
-        processDataset(done, allData, tableName);
-    },
-	function(done) { 
-        var tableName = 'Gardabook.Tag';
-        console.log('Generated',Object.keys(allData[tableName]).length, 'Tag');
-        processDataset(done, allData, tableName);
-    },
-	function(done) { 
-        var tableName = 'Gardabook.Availability';
-        console.log('Generated',Object.keys(allData[tableName]).length, 'Availability');
-        processDataset(done, allData, tableName);
-    },
-    function(done) { 
-        var tableName = 'Gardabook.Catalogue';
-        console.log('Generated',Object.keys(allData[tableName]).length, 'Catalogue');
-        processDataset(done, allData, tableName);
-    },
-    function(done) { 
-        var tableName = 'Gardabook.Bookings';
-        console.log('Generated',Object.keys(allData[tableName]).length, 'Bookings');
-        processDataset(done, allData, tableName);
-    },
-	function(done) { 
-        var tableName = 'Gardabook.Enrollment';
-        console.log('Generated',Object.keys(allData[tableName]).length, 'Enrollment');
-        processDataset(done, allData, tableName);
-    }
-);
+  function(done) {
+    var tableName = "Gardabook.User"
+    console.log("Generated", Object.keys(allData[tableName]).length, "User")
+    processDataset(done, allData, tableName)
+  },
+  function(done) {
+    var tableName = "Gardabook.Tag"
+    console.log("Generated", Object.keys(allData[tableName]).length, "Tag")
+    processDataset(done, allData, tableName)
+  },
+  function(done) {
+    var tableName = "Gardabook.Availability"
+    console.log(
+      "Generated",
+      Object.keys(allData[tableName]).length,
+      "Availability"
+    )
+    processDataset(done, allData, tableName)
+  },
+  function(done) {
+    var tableName = "Gardabook.Catalogue"
+    console.log(
+      "Generated",
+      Object.keys(allData[tableName]).length,
+      "Catalogue"
+    )
+    processDataset(done, allData, tableName)
+  },
+  function(done) {
+    var tableName = "Gardabook.Bookings"
+    console.log("Generated", Object.keys(allData[tableName]).length, "Bookings")
+    processDataset(done, allData, tableName)
+  },
+  function(done) {
+    var tableName = "Gardabook.Enrollment"
+    console.log(
+      "Generated",
+      Object.keys(allData[tableName]).length,
+      "Enrollment"
+    )
+    processDataset(done, allData, tableName)
+  }
+)
 // END SETUP WORKLOADS
 
-function startProcessingDataParallel() {
-    async.parallel(work, function(err, data) {
-        if (err)
-            console.log('Unexpected Error: ', err, err.stack);
-    });
-}
-
 function startProcessingDataSeries() {
-    async.series(work, function(err, data) {
-        if (err)
-            console.log('Unexpected Error: ', err, err.stack);
-    });
+  async.series(work, function(err, data) {
+    if (err) console.log("Unexpected Error: ", err, err.stack)
+  })
 }
 
-// startProcessingDataParallel();
-startProcessingDataSeries();
+startProcessingDataSeries()
 
 function executeBatchPut(params) {
-    var request = dynamodb.batchWriteItem(params);
-    return request.promise();
+  var request = dynamodb.batchWriteItem(params)
+  return request.promise()
 }
 
 function processDataset(done, allData, tableName, UnprocessedItems) {
-    var params = buildParams();
-    var requestItemCount = params.RequestItems[tableName].length;
-    if (requestItemCount === 0) {
-        done();
-        return;
+  var params = buildParams()
+  var requestItemCount = params.RequestItems[tableName].length
+  if (requestItemCount === 0) {
+    done()
+    return
+  }
+
+  executeBatchPut(params)
+    .then(processBatchPutResponse)
+    .catch(handleError)
+
+  function processBatchPutResponse(response) {
+    var request
+
+    if (!response.UnprocessedItems || !response.UnprocessedItems.length) {
+      console.log("   Wrote", requestItemCount, "items to table", tableName)
+      processDataset(done, allData, tableName)
+      return
     }
 
-    executeBatchPut(params)        
-        .then(processBatchPutResponse)
-        .catch(handleError)
+    var unprocessedCount = Object.keys(response.UnprocessedItems[tableName])
+      .length
+    if (unprocessedCount > 0) {
+      console.log(
+        "   Wrote",
+        requestItemCount - unprocessedCount,
+        "items to table",
+        tableName,
+        "(Unable to process",
+        unprocessedCount,
+        "items)"
+      )
+      processDataset(done, allData, tableName, response.UnprocessedItems)
+    }
+  }
 
-    function processBatchPutResponse(response) {
-        var request;
+  function buildParams() {
+    var dataSet = allData[tableName]
+    var params = {
+      RequestItems: {},
+      ReturnConsumedCapacity: "TOTAL"
+    }
+    params.RequestItems[tableName] = []
 
-        if (!response.UnprocessedItems || !response.UnprocessedItems.length) {
-            console.log('   Wrote', requestItemCount, 'items to table', tableName);
-            processDataset(done, allData, tableName);            
-            return;
-        }
-        
-        var unprocessedCount = Object.keys(response.UnprocessedItems[tableName]).length;
-        if (unprocessedCount > 0) {
-            console.log('   Wrote', (requestItemCount - unprocessedCount), 'items to table', tableName, '(Unable to process',unprocessedCount,'items)');
-            processDataset(done, allData, tableName, response.UnprocessedItems);
-        }
+    if (UnprocessedItems) {
+      params.RequestItems = UnprocessedItems
     }
 
-    function buildParams() {
-        var dataSet = allData[tableName];
-        var params = {
-            RequestItems: {},
-            "ReturnConsumedCapacity": "TOTAL"
-        };
-        params.RequestItems[tableName] = [];
+    for (var id in dataSet) {
+      if (params.RequestItems[tableName].length === 25) break
 
-        if (UnprocessedItems) {
-            params.RequestItems = UnprocessedItems;
+      var request = {
+        PutRequest: {
+          Item: dataSet[id]
         }
+      }
 
-        for (var id in dataSet) {
-            if (params.RequestItems[tableName].length === 25)
-                break;
-
-            var request = {
-                "PutRequest": {
-                    "Item": dataSet[id]
-                }
-            };
-
-            params.RequestItems[tableName].push(request);
-            delete dataSet[id];
-        }
-        return params;
+      params.RequestItems[tableName].push(request)
+      delete dataSet[id]
     }
-    
-    function handleError(err) {
-        console.log('   Error:', err, err.stack);
-        if (params.RequestItems[tableName].length !== 0) {
-            processDataset(done, allData, tableName, params.RequestItems);
-        }
-    }
+    return params
+  }
 
+  function handleError(err) {
+    console.log("   Error:", err, err.stack)
+    if (params.RequestItems[tableName].length !== 0) {
+      processDataset(done, allData, tableName, params.RequestItems)
+    }
+  }
 }
-
-
