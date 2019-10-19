43eae554cf62c028ec8baab5e2d80df885a9402c
Jiang Jie <jiangjie201407@gmail.com>
2019-10-19 19:23:38 +0800

    Update userValidator.js

--- gardabook-services/model/entities/validators/userValidator.js
+++ gardabook-services/model/entities/validators/userValidator.js
@@ -1,6 +1,87 @@
-// TODO: Validate availability props
-const validateProps = props => {
-  return true
+const possiblePropKeys = [
+  "pKey",
+  "sKey",
+  "role",
+  "username",
+  "firstName",
+  "lastName",
+  "email",
+  "phone",
+  "dob",
+  "gender",
+  "address",
+  "status",
+  "createdAt",
+  "updatedAt"
+]
+
+const requiredPropKeysForCreate = [
+  "pKey",
+  "sKey",
+  "role",
+  "username",
+  "firstName",
+  "lastName",
+  "email",
+  "phone",
+  "dob",
+  "gender",
+  "address",
+  "status",
+  "createdAt",
+  "updatedAt"
+]
+
+const requiredPropKeysForRead = ["pKey"]
+
+const requiredPropKeysForUpdate = ["pKey", "sKey", "updatedAt"]
+
+const requiredPropKeysForDelete = ["pKey", "sKey"]
+
+const requiredPropKeyEnum = {
+  CREATE: 1,
+  READ: 2,
+  UPADTE: 3,
+  DELETE: 4,
+  properties: {
+    1: {
+      values: requiredPropKeysForCreate
+    },
+    2: {
+      values: requiredPropKeysForRead
+    },
+    3: {
+      values: requiredPropKeysForUpdate
+    },
+    4: {
+      values: requiredPropKeysForDelete
+    }
+  }
+};
+
+const validateProps = (props, validateOption) => {
+  const propKeys = Object.keys(props)
+  var requiredKeys = [...requiredPropKeyEnum.properties[validateOption].values]
+  let correctProps = true
+
+  propKeys.forEach(key => {
+    if (!possiblePropKeys.includes(key)) {
+      correctProps = false
+    } else {
+      const index = requiredKeys.indexOf(key)
+      requiredKeys.splice(index, 1)
+    }
+  })
+
+  if (requiredKeys.length > 0) {
+    return false
+  }
+
+  if (!correctProps) {
+    return false
+  }
+
+  return true;
 }
 
-module.exports = { validateProps }
+module.exports = { validateProps, requiredPropKeyEnum }
