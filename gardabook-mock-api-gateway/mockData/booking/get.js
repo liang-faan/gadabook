5849e87cdaad7cb1213d299962ddc24ff988b125
stonefruit <jeromeng888@gmail.com>
2019-10-29 06:42:26 +0800

    updatem mockdata

--- gardabook-mock-api-gateway/mockData/booking/get.js
+++ gardabook-mock-api-gateway/mockData/booking/get.js
@@ -32,7 +32,7 @@ const data1 = {
           status: 'Active',
         },
         {
-          tagId: 6,
+          tagId: 7,
           descritpion: 'descritpion',
           status: 'Active',
         },
@@ -90,6 +90,99 @@ const data1 = {
   status: 'New',
 }
 
+const data2 = {
+  amount: 1,
+  submitDate: '2000-01-23T04:56:07.000+00:00',
+  bookingDate: '2000-01-24T04:56:07.000+00:00',
+  availability: {
+    date: '2000-01-23',
+    availablityId: 0,
+    catalogue: {
+      venue: 'venue',
+      catalogueId: 0,
+      address: 'address',
+      city: 'city',
+      rateUnit: 'Minute',
+      remark: 'remark',
+      provider: {
+        firstName: 'firstName',
+        lastName: 'lastName',
+        userStatus: 5,
+        phone: 'phone',
+        userId: '1',
+        email: 'email',
+        username: 'username',
+      },
+      rate: 5.637376656633329,
+      terms: 'terms',
+      name: 'NUS ISS Meeting Room#5',
+      currency: 'SGD',
+      tag: [
+        {
+          tagId: 6,
+          descritpion: 'descritpion',
+          status: 'Active',
+        },
+        {
+          tagId: 7,
+          descritpion: 'descritpion',
+          status: 'Active',
+        },
+      ],
+      status: 'Open',
+    },
+  },
+  paymentDate: '2000-01-23T04:56:07.000+00:00',
+  complete: false,
+  user: {
+    firstName: 'firstName',
+    lastName: 'lastName',
+    userStatus: 5,
+    phone: 'phone',
+    userId: '1',
+    email: 'email',
+    username: 'username',
+  },
+  bookingId: 1,
+  catalogue: {
+    venue: 'venue',
+    catalogueId: 0,
+    address: 'address',
+    city: 'city',
+    rateUnit: 'Minute',
+    remark: 'remark',
+    provider: {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      userStatus: 5,
+      phone: 'phone',
+      userId: '1',
+      email: 'email',
+      username: 'username',
+    },
+    rate: 5.637376656633329,
+    terms: 'terms',
+    name: 'NUS ISS Meeting Room#5',
+    currency: 'SGD',
+    tag: [
+      {
+        tagId: 6,
+        descritpion: 'descritpion',
+        status: 'Active',
+      },
+      {
+        tagId: 6,
+        descritpion: 'descritpion',
+        status: 'Active',
+      },
+    ],
+    status: 'Open',
+  },
+  createDate: '2000-01-23T04:56:07.000+00:00',
+  status: 'New',
+}
+
 module.exports = {
   data1,
+  data2,
 }
--- gardabook-mock-api-gateway/mockData/booking/list.js
+++ gardabook-mock-api-gateway/mockData/booking/list.js
@@ -1,4 +1,9 @@
-const data1 = {}
+const getBooking = require('./get')
+
+const booking1 = getBooking.data1
+const booking2 = getBooking.data2
+
+const data1 = { bookings: [booking1, booking2] }
 
 module.exports = {
   data1,
