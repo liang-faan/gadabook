4db48093d9e598eae5feb47e0deff203a042f1b5
Denny Liang Faan <liang.faan@gmail.com>
2019-11-02 19:29:46 +0800

    update controller to adopt Lambda function

--- gardabook-services/controllers/Booking.js
+++ gardabook-services/controllers/Booking.js
@@ -6,75 +6,89 @@ var Booking = require('../service/BookingService');
 module.exports.getUserBooking = function getUserBooking(req, res, next) {
   var xIntRole = '';
   var userId = '';
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     userId = req.swagger.params['userId'].value;
+    apiResponse=res;
   } else {
+    console.log(req);
     xIntRole = '';
-    userId = req.params['userId'].value;
+    userId = req.query.userId;
+    apiResponse=next;
   }
   Booking.getUserBooking(xIntRole, userId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.deleteBooking = function deleteBooking(req, res, next) {
   var xIntRole = '';
   var bookingId = '';
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     bookingId = req.swagger.params['bookingId'].value;
+    apiResponse = res;
   } else {
+    console.log(req);
     xIntRole = '';
-    bookingId = req.params['bookingId'].value;
+    bookingId= req.pathParameters['bookingId'];
+    apiResponse=next;
   }
   Booking.deleteBooking(xIntRole, bookingId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.getBooking = function getBooking(req, res, next) {
   var xIntRole = '';
   var bookingId = '';
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     bookingId = req.swagger.params['bookingId'].value;
+    apiResponse=res;
   } else {
-    xIntRole = req.params['x-int-role'].value;
-    bookingId = req.params['bookingId'].value;
+    xIntRole = '';
+    bookingId = req.pathParameters['bookingId'];
+    apiResponse=next;
   }
   Booking.getBooking(xIntRole, bookingId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.createUserBooking = function createUserBooking(req, res, next) {
   var xIntRole = '';
   var body;
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     body = req.swagger.params['body'].value;
+    apiResponse = res;
   } else {
-    xIntRole = req.params['x-int-role'].value;
-    body = req.params['body'].value;
+    // xIntRole = req.params['x-int-role'].value;
+    body = req.body;
+    apiResponse = next;
   }
   Booking.createUserBooking(xIntRole, body)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
--- gardabook-services/controllers/Catalogue.js
+++ gardabook-services/controllers/Catalogue.js
@@ -6,118 +6,136 @@ var Catalogue = require('../service/CatalogueService');
 module.exports.addCatalogue = function addCatalogue(req, res, next) {
   var xIntRole = '';
   var body;
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     body = req.swagger.params['body'].value;
+    apiResponse = res;
   } else {
     // xIntRole = '';
     body = req.body;
+    apiResponse=next;
     
   }
   console.log(body);
   Catalogue.addCatalogue(xIntRole, body)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.deleteCatalogue = function deleteCatalogue(req, res, next) {
   var xIntRole = '';
   var catalogueId = '';
-  var api_key = '';
+  // var api_key = '';
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     catalogueId = req.swagger.params['catalogueId'].value;
-    api_key = req.swagger.params['api_key'].value;
+    // api_key = req.swagger.params['api_key'].value;
+    apiResponse = res;
   } else {
-    xIntRole = req.params['x-int-role'].value;
-    catalogueId = req.params['catalogueId'].value;
-    api_key = req.params['api_key'].value;
+    // xIntRole = req.params['x-int-role'].value;
+    catalogueId = req.pathParameters['catalogueId'];
+    // api_key = req.params['api_key'].value;
+    apiResponse = next;     
   }
   Catalogue.deleteCatalogue(xIntRole, catalogueId, api_key)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.readCatalogueByTags = function readCatalogueByTags(req, res, next) {
   var xIntRole = '';
   var tags = '';
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     tags = req.swagger.params['tags'].value;
+    apiResponse = res;
   } else {
-    xIntRole = req.params['x-int-role'].value;
-    tags = req.params['tags'].value;
+    // xIntRole = req.params['x-int-role'].value;
+    tags = req.query.tags;
+    apiResponse=next;
   }
   Catalogue.readCatalogueByTags(xIntRole, tags)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.readCatalogueByAvailabilityId = function readCatalogueByAvailabilityId(req, res, next) {
   var xIntRole = '';
   var availabilityId = '';
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     availabilityId = req.swagger.params['availabilityId'].value;
+    apiResponse = res;
   } else {
-    xIntRole = req.swagger.params['x-int-role'].value;
-    availabilityId = req.swagger.params['availabilityId'].value;
+    // xIntRole = req.swagger.params['x-int-role'].value;
+    availabilityId = req.pathParameters['availabilityId'];
+    apiResponse = next;
   }
   Catalogue.readCatalogueByAvailabilityId(xIntRole, availabilityId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.readCatalogue = function readCatalogue(req, res, next) {
   var xIntRole = '';
   var catalogueId = '';
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     catalogueId = req.swagger.params['catalogueId'].value;
+    apiResponse =res;
   } else {
-    xIntRole = req.params['x-int-role'].value;
-    catalogueId = req.params['catalogueId'].value;
+    // xIntRole = req.params['x-int-role'].value;
+    catalogueId = req.pathParameters['catalogueId'];
+    apiResponse = next;
   }
   Catalogue.readCatalogue(xIntRole, catalogueId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
 
 module.exports.updateCatalogue = function updateCatalogue(req, res, next) {
   var xIntRole = '';
   var body;
+  var apiResponse;
   if (process.env.NODE_ENV == 'development') {
     xIntRole = req.swagger.params['x-int-role'].value;
     body = req.swagger.params['body'].value;
+    apiResponse = res;
   } else {
-    xIntRole = req.params['x-int-role'].value;
-    body = req.params['body'].value;
+    // xIntRole = req.params['x-int-role'].value;
+    body = req.body;
+    apiResponse = next;
   }
   Catalogue.updateCatalogue(xIntRole, body)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(apiResponse, response);
     });
 };
