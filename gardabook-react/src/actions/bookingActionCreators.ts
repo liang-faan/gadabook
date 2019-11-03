aeb9bfdc5650486b3e99ea65bb89e1e633fabf24
stonefruit <jeromeng888@gmail.com>
2019-11-03 13:33:12 +0800

    update react app rooturl

--- gardabook-react/src/actions/bookingActionCreators.ts
+++ gardabook-react/src/actions/bookingActionCreators.ts
@@ -3,8 +3,9 @@ import axios from 'axios'
 import { rootUrl } from '../settings'
 import { BOOKING_UPDATE_BOOKING_LIST } from './types'
 
+const userId = 'User_BD49B658-122B-44BE-8B2C-60846EC5864C'
 export const getBookingList = () => dispatch => {
-  axios.get(`${rootUrl}/booking`).then(res => {
+  axios.get(`${rootUrl}/booking/?userId=${userId}`).then(res => {
     dispatch({
       type: BOOKING_UPDATE_BOOKING_LIST,
       payload: res.data.bookings,
