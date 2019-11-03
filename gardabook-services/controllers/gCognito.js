384efbded6150791e76768f447791b2f2e86419f
Denny Liang Faan <liang.faan@gmail.com>
2019-11-03 17:36:08 +0800

    update catalogue

--- gardabook-services/controllers/gCognito.js
+++ gardabook-services/controllers/gCognito.js
@@ -2,19 +2,43 @@
 // var utils = require('../utils/writer.js')
 var gcognito = require('../service/CognitoService')
 
-module.exports.getCognitoTokenFromGoogleCode = function getCognitoTokenFromGoogleCode(req,res,cb) {
-    const code = req.query.code
-    const response = req.Records[0].cf.response;
-    const headers = response.headers;
+module.exports.getCognitoTokenFromGoogleCode = function getCognitoTokenFromGoogleCode(event, context, cb) {
+    const code = event.query.code
+    // const response = event.Records[0].cf.response;
+    // const headers = response.headers;
+
     gcognito.getCognitoTokenFromGoogleCode(code)
         .then(function (token) {
-            headers['Authorization']=[{key:"Authorization", value: token}];
+            var responseCode = 200;
+            var response = {
+                statusCode: responseCode,
+                headers: {
+                    "Authorization": token
+                }
+            };
             // response.status = '200'
-            cb(null, {response});
+            cb(null, {
+                response
+            });
         })
         .catch(function (gCognitoResp) {
-            cb(null, {response});
+            var responseCode = 401;
+            var response = {
+                statusCode: responseCode,
+                headers: {
+                    "Authorization": token
+                }
+            };
+            cb(null, {
+                response
+            });
         })
-        
+
 
 }
+
+
+
+
+
+context.succeed(response);
