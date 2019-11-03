64147fc32e39246cfa7e5ab4bad95f4bc382ff24
stonefruit <jeromeng888@gmail.com>
2019-11-03 12:35:19 +0800

    update gCognito controller

--- gardabook-services/controllers/gCognito.js
+++ gardabook-services/controllers/gCognito.js
@@ -1,12 +1,18 @@
-'use strict';
-var gcognito=require("../service/CognitoService");
+'use strict'
+var gcognito = require('../service/CognitoService')
 
-module.exports.getCognitoTokenFromGoogleCode = function getCognitoTokenFromGoogleCode (req, res, next){
-    gcognito.getCognitoTokenFromGoogleCode(req)
-    .then(function (response) {
-        utils.writeJson(res, response);
-      })
-      .catch(function (response) {
-        utils.writeJson(res, response);
-      });
+module.exports.getCognitoTokenFromGoogleCode = function getCognitoTokenFromGoogleCode(
+  req,
+  res,
+  next
+) {
+  const code = req.query.code
+  gcognito
+    .getCognitoTokenFromGoogleCode(code)
+    .then(function(response) {
+      utils.writeJson(res, response)
+    })
+    .catch(function(response) {
+      utils.writeJson(res, response)
+    })
 }
