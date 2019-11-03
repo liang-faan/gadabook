2a2839921a145faeddd9ecde79864b1d0eafc173
stonefruit <jeromeng888@gmail.com>
2019-11-03 12:26:27 +0800

    update all user pools in code to new one

--- AWS-Cognito_Sample Project/js/config.js
+++ AWS-Cognito_Sample Project/js/config.js
@@ -1,8 +1,7 @@
 window._config = {
-    cognito: {
-        userPoolId: 'ap-southeast-1_aBjWtf10v', 
-        region: 'ap-southeast-1',
-		clientId: '1farfu2bjp9gdf746hjuvs0q0e' 
-    },
-};
-
+  cognito: {
+    userPoolId: 'ap-southeast-1_n90l7WmwP',
+    region: 'ap-southeast-1',
+    clientId: '1farfu2bjp9gdf746hjuvs0q0e',
+  },
+}
